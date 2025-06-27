51f0411ecb14fb4fa39b50b57322b5767804d7a7
HwaRang Kim <khrang54@gmail.com>
2025-06-27 13:27:01 +0900

    [upload] imageshop project create

---
+++ src/main/java/com/kh/KhrImageShopApplication.java
@@ -0,0 +1,13 @@
+package com.kh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhrImageShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KhrImageShopApplication.class, args);
+	}
+
+}
